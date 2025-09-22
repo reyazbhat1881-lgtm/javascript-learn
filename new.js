288afb115203e7c4df49da92481320ef53b570e7
Reyaz Ahamd Bhat <reyazbhat1881@gmail.com>
2025-09-22 14:39:04 +0530

    added some new code lines

--- new.js
+++ new.js
@@ -9,4 +9,27 @@ console.log(roll);
 let name2 = "tifail" 
 console.log(name2);
 
+let user = {
+    email: "reyazbhat1881@gmailcom",
+    upi: "user@123"
+}
+
+let user2 = user
+
+user2.email = "new@gmail.com"
+
+console.log(user);
+console.log(user2);
+
+const name10 = "Reyaz"
+const rollNo = 232113
+
+// console.log(name10 + rollNo + "Hello");
+
+console.log(`hello my name is  ${name} and my roll number is ${rollNo}`);
+
+console.log(rollNo);
+
+
+
 
