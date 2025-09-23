ffdb4fcfcfc50a4b463c07c801112d7676df5895
Reyaz Ahamd Bhat <reyazbhat1881@gmail.com>
2025-09-23 21:55:03 +0530

    learned about some string method

--- new.js
+++ new.js
@@ -1,62 +1,87 @@
-const name = "reyaz"
+// const name = "reyaz"
 
-console.log(name)
+// console.log(name)
 
-let roll = 29;
+// let roll = 29;
 
-console.log(roll);
+// console.log(roll);
 
-let name2 = "tifail" 
-console.log(name2);
+// let name2 = "tifail" 
+// console.log(name2);
 
-let user = {
-    email: "reyazbhat1881@gmailcom",
-    upi: "user@123"
-}
+// let user = {
+//     email: "reyazbhat1881@gmailcom",
+//     upi: "user@123"
+// }
 
-let user2 = user
+// let user2 = user
 
-user2.email = "new@gmail.com"
+// user2.email = "new@gmail.com"
 
-console.log(user);
-console.log(user2);
+// console.log(user);
+// console.log(user2);
 
-const name10 = "Reyaz"
-const rollNo = 232113
+// const name10 = "Reyaz"
+// const rollNo = 232113
 
-// console.log(name10 + rollNo + "Hello");
+// // console.log(name10 + rollNo + "Hello");
 
-console.log(`hello my name is  ${name} and my roll number is ${rollNo}`);
+// console.log(`hello my name is  ${name} and my roll number is ${rollNo}`);
 
-console.log(rollNo);
+// console.log(rollNo);
 
-let xxx = 2000;
+// let xxx = 2000;
 
-console.log(xxx);
+// console.log(xxx);
 
-const gameName = new String (`Reyaz`)
+// const gameName = new String (`Reyaz`)
 
-console.log(gameName.length)
-console.log(gameName.toLowerCase());
-console.log(gameName.charAt(3));
-console.log(gameName.indexOf('z'));
+// console.log(gameName.length)
+// console.log(gameName.toLowerCase());
+// console.log(gameName.charAt(3));
+// console.log(gameName.indexOf('z'));
 
-const newString = gameName.substring(0,3) 
+// const newString = gameName.substring(0,3) 
 
-// this does not include the last character of the string it ignores it
+// // this does not include the last character of the string it ignores it
 
-console.log(newString)
+// console.log(newString)
 
-const anotherString = gameName.slice(-3, 4)
-console.log(anotherString);
+// const anotherString = gameName.slice(-3, 4)
+// console.log(anotherString);
 
-const newString2 = "      Reyaz   "
-console.log(newString2);
+// const newString2 = "      Reyaz   "
+// console.log(newString2);
 
-console.log(newString2.trim());
+// console.log(newString2.trim());
 
 
+// const neww = 'reyaz ahmad bhat'
 
+// console.log(neww.replace('y', '0'))
+
+// console.log(neww.includes('r'));
+
+// console.log(neww.split('a'));
+// console.log(neww.charCodeAt('r'));
+
+// console.log(neww.charAt('3'));
+
+// const str1 = 'Hello'
+// const str2 = 'World'
+
+// console.log(str1.concat(str2));
+
+// const str3 = str1 + str2;
+// console.log(str3);
+
+// const index = 2;
+// console.log(`the character at index ${'2'} is ${str1.charAt('2')}`);
+
+
+const learn = 'Hello, today im learning about the javascript strings. Strings in javascript are very important for mastery'
+
+console.log(learn.slice(-10));
 
 
 
