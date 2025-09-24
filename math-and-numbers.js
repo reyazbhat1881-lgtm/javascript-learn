026af6d8ef109197b7205b8efd9f6548aee0b14b
Reyaz Ahamd Bhat <reyazbhat1881@gmail.com>
2025-09-24 13:51:04 +0530

    lernded about the math libarry

--- math-and-numbers.js
+++ math-and-numbers.js
@@ -1,5 +1,41 @@
-const score = new Number (400)
-const character2 = 'hello'
+// const score = new Number (400)
+// const character2 = 'hello'
 
-console.log(score);
-console.log(character2);
+// console.log(score);
+// console.log(character2);
+
+// console.log(score.toString().charAt(2));
+
+
+// console.log(score.toFixed(1));
+
+// console.log(score.MAX_VALUE)
+
+// console.log(Math);
+
+
+console.log(Math.abs);
+console.log(Math.abs(-38838));
+
+console.log(Math.round(5.9));
+
+console.log(Math.ceil(4.4));
+console.log(Math.floor(4.4)); 
+
+console.log(Math.random(1,3,21,4,535,35,));
+
+console.log(Math.random()*10)+1;
+
+const min = 10;
+const max = 20;
+
+console.log(Math.ceil (Math.random(min-max) +min));
+
+
+
+
+
+
+
+
+
