47bf3da83629c3c81eb223adf5bc910a364b9942
Reyaz Ahamd Bhat <reyazbhat1881@gmail.com>
2025-09-24 22:39:52 +0530

    lerned about the array methods

---
+++ araays.js
@@ -0,0 +1,25 @@
+//arays
+
+let arr = [ 1,3,6,4,9,7]
+
+// console.log(arr[1]);
+
+// arr.push(22)
+
+// console.log(arr);
+
+// arr.pop()
+
+// console.log(arr);
+
+// // arr.unshift(7)
+// arr.shift()
+
+// console.log(arr.includes(3));
+
+// console.log(arr);
+
+const m1 = arr.splice(2,4)
+
+console.log(m1);
+
