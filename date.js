e33533ab73e829593167a40582eda7d89c0aab52
Reyaz Ahamd Bhat <reyazbhat1881@gmail.com>
2025-09-24 21:16:06 +0530

    lesrned about the dates

---
+++ date.js
@@ -0,0 +1,29 @@
+//dates
+
+// let d = new Date()
+
+// console.log(d.toDateString());
+// console.log(d.getDate())
+
+// let x = new Date(2025,5, 3)
+
+// console.log(x.toDateString());
+
+
+// let mt = Date.now()
+
+// console.log(mt);
+
+// console.log(Date.now()/1000);
+
+// console.log(Math.floor(Date.now()/1000));
+
+
+let newDate = new Date()
+
+console.log(newDate);
+
+console.log(newDate.getMonth());
+
+// `${newDate.getMonth()} and the date is `
+
