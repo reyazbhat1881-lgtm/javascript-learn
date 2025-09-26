2377443b317cbb3d234470ce274e0fa31d50dedc
Reyaz Ahamd Bhat <reyazbhat1881@gmail.com>
2025-09-26 21:56:48 +0530

    added some araays methods

--- araays.js
+++ araays.js
@@ -1,6 +1,6 @@
 //arays
 
-let arr = [ 1,3,6,4,9,7]
+// let arr = [ 1,3,6,4,9,7]
 
 // console.log(arr[1]);
 
@@ -19,10 +19,38 @@ let arr = [ 1,3,6,4,9,7]
 
 // console.log(arr);
 
-const m1 = arr.splice(2,4)
+// const m1 = arr.splice(2,4)
 
-console.log(m1);
+// console.log(m1);
 
 //rsgmkggccvgnvbjjdj
 
 // hello
+
+// const friends_college = ["Reyaz, Tufail, Azhar"]
+// const friends_home = ["Abid, Adil, Farooq "]
+// friends_college.push(friends_home)
+
+// console.log(friends_college);
+
+// let a = 10;
+// let b = 24;
+// let c = 5;
+
+// let x = a + b + c;
+// console.log(x);
+
+
+// console.log(Array.from("reyaz"));
+
+// let arrr = "Reyaz"
+
+// console.log(Array.isArray(arrr));
+
+let score1 = 100;
+let score2 = 200;
+let score3 = 300;
+
+console.log(Array.of(score1,score2,score3));
+
+
