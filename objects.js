f685cf9610e36bf66060287484b942df0ff8b2a0
Reyaz Ahamd Bhat <reyazbhat1881@gmail.com>
2025-09-26 22:06:04 +0530

    lerned about the basics of the objects

---
+++ objects.js
@@ -0,0 +1,23 @@
+const user = {
+
+    name: "Reyaz",
+    roll_number: 232113,
+    Residence: "Wailoo",
+    Phone: 34893,
+}
+
+console.log(user);
+
+console.log(user.roll_number);
+
+console.log(typeof user.Phone);
+
+
+
+user.new = function()
+{
+    console.log();
+    
+}
+
+console.log(user);
