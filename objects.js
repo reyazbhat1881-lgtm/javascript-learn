43997655384764566ee289ed578ae2ae85fcd64b
Reyaz Ahamd Bhat <reyazbhat1881@gmail.com>
2025-10-05 12:23:21 +0530

    learned more about objects

--- objects.js
+++ objects.js
@@ -1,23 +1,82 @@
-const user = {
+// // const user = {
 
-    name: "Reyaz",
-    roll_number: 232113,
-    Residence: "Wailoo",
-    Phone: 34893,
-}
+// //     name: "Reyaz",
+// //     roll_number: 232113,
+// //     Residence: "Wailoo",
+// //     Phone: 34893,
+// // }
 
-console.log(user);
+// // console.log(user);
 
-console.log(user.roll_number);
+// // console.log(user.roll_number);
 
-console.log(typeof user.Phone);
+// // console.log(typeof user.Phone);
+
+// // user.new = function()
+// // {
+// //     console.log();
+    
+// // }
+
+// // console.log(user);
+
+// const user = {}
+
+// user.id="1343"
+// user.name = "reyaz"
+// console.log(user);
+
+// const reguleruser = {
+
+//     email: "reyaz@gmail.com",
+//     fullname: {
+
+//         userfullname: {
+//             firstname: "reyaz",
+//             lastname: "bhar"
+
+//         }
+
+//     }
+// // }
+
+// // console.log(reguleruser.fullname.userfullname.firstname);
+
+// const obj1 = {
+//     1: 'a',
+//     2: 'b'
+// }
+
+// const obj2 = {
+//     1: 'c',
+//     2: 'd'
+// }
+
+// // const obj3 = {
+// //     obj1, obj2
+// // }
+
+// // const obj3 = Object.assign({},obj1,obj2)
+
+// const obj3 = { ...obj1, ...obj2}
+// console.log(obj3);
 
 
+const tinder = {
 
-user.new = function()
-{
-    console.log();
+    name: {
+
+        first: 'reyz',
+        last: 'bhat'
+    }
+
     
 }
 
-console.log(user);
+console.log(tinder);
+console.log(Object.values(tinder));
+console.log(Object.keys(tinder));
+console.log(Object.entries(tinder));
+
+console.log(tinder.hasOwnProperty('name'));
+
