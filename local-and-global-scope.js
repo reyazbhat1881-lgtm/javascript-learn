09a80fd34a7df6112f583ba11a12fe5d97014adb
Reyaz Ahamd Bhat <reyazbhat1881@gmail.com>
2025-10-06 18:01:48 +0530

    learned about the scopes of the variables in the fuctions

---
+++ local-and-global-scope.js
@@ -0,0 +1,51 @@
+// let a = 99;
+
+// console.log(a);
+
+let c = 33
+
+if (true){
+    let a =22
+    const x = 33
+    
+    console.log(a,x, c);
+
+}
+
+function one()
+{
+    const username = 'reyaz'
+    console.log(username)
+
+    function two()
+    {
+      const subject = 'information technology'
+      console.log(subject);
+      console.log(username);
+      
+      
+    }
+    two()
+
+    
+}
+one()
+
+if(true){
+    const username2 = 'Reyaz'
+    if(username2 === 'Reyaz')
+    {
+        const  address = 'wailoo'
+        console.log(username2 + address);
+        
+    }
+}
+
+// ************ intresting **************
+
+const add = function(num){
+    return num + 1
+
+}
+
+console.log(add(3))
