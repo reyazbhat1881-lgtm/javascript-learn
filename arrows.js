033f38004b3b102eee84c8d7812173e59098c0a4
Reyaz Ahamd Bhat <reyazbhat1881@gmail.com>
2025-10-06 21:47:41 +0530

    learned about the iif

---
+++ arrows.js
@@ -0,0 +1,36 @@
+// const userData = {
+//     username: 'reyaz',
+//     userRoll: 26387,
+
+//     welcomeMessage: function() {
+//         console.log(`${this.username}, welcome to this website`);
+//     console.log(this);
+    
+//     }
+// };
+
+// userData.welcomeMessage();
+
+// userData.username = 'tufail'
+// console.log(userData.username)
+// console.log(this);
+
+// const shadi = () =>{
+//     let groomName = 'reyaz';
+    
+//     console.log(this);
+    
+    
+
+// }
+
+// shadi()
+
+// ************ Arrow functions ***************
+
+/* if we use curly braces then we will to have use the return, other we can use the patenthesis then we dont have to use the return statement */
+
+const addtwo = (num1, num2) => (num1+ num2)
+
+
+console.log(addtwo(3,4));
---
+++ iif.js
@@ -0,0 +1,11 @@
+/* immediately invoked functions */
+
+// (function name(){
+
+//     console.log('Hey, there');
+    
+// }) () 
+
+( (name2) => {
+    console.log(`${name2} yes you are connectd to the server`)
+}) ('reyaz')
