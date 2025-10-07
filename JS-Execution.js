417fd4023babea38bf925867b62158a81e900052
Reyaz Ahamd Bhat <reyazbhat1881@gmail.com>
2025-10-07 18:42:07 +0530

    learned about the javascript execution

---
+++ JS-Execution.js
@@ -0,0 +1,19 @@
+let val1 = 2
+let val2 = 3
+function addtwo(num1, num2){
+    let total = num1 + num2
+    return total
+}
+let result1 = addtwo(val1, val2)
+let result2 = addtwo(3,4)
+
+console.log(result1,result2);
+
+// ***** 1.... Global Execution *****
+//-----> this
+
+
+// ***** 2.... Memory Phase *****
+
+// ***** 3.... Execution Phase *****
+//............ new variable encvirnoment + execution thread
