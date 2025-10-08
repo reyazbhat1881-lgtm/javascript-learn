f033eccfe3658a67237bfad7fe3304e32f987516
Reyaz Ahamd Bhat <reyazbhat1881@gmail.com>
2025-10-08 22:53:32 +0530

    basic learning about the is and else statements

---
+++ logic-control/one.js
@@ -0,0 +1,17 @@
+// ....If Statement .....
+
+// if (2<=2){
+//     console.log("done");
+    
+// }
+
+const temperature = 55;
+if (temperature < 50){
+    console.log("temperature is lessthan 50");
+    
+}
+
+else{
+    console.log("temperature is greaterthan 50");
+    
+}
