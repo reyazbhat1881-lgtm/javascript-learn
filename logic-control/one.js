4f623d5d349ac4303d75f864dab7dbcc0c54ac67
Reyaz Ahamd Bhat <reyazbhat1881@gmail.com>
2025-10-09 12:11:47 +0530

    learned more about the if eelse

--- logic-control/one.js
+++ logic-control/one.js
@@ -5,13 +5,58 @@
     
 // }
 
-const temperature = 55;
-if (temperature < 50){
-    console.log("temperature is lessthan 50");
+// const temperature = 55;
+// if (temperature < 50){
+//     console.log("temperature is lessthan 50");
     
-}
+// }
 
-else{
-    console.log("temperature is greaterthan 50");
+// else{
+//     console.log("temperature is greaterthan 50");
+    
+// }
+
+// let balance = 500;
+
+// if( balance <=500){
+//     console.log("is lessthan");
+    
+// }
+
+// else if (balance >= 500){
+//     console.log("is greater than");
+    
+// }
+
+// else{
+//     console.log("is equal");
+    
+// }
+
+// let a = 3;
+// let b = 4;
+// let c = 33;
+
+// if(a > b &&c){
+//     console.log("a is greater");
+    
+// }
+
+// else if(b > a && c){
+//     console.log("b is greater");
+    
+// }
+
+// else if(c > a && b){
+//     console.log("c is grater");
+    
+// }
+
+const hasExperience = true
+const hasMasters = false
+
+if(hasExperience || hasMasters)
+{
+    console.log("eligible");
     
 }
