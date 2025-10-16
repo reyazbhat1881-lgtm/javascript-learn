da1d6692acea90ba1d4c4f9a12148be819825620
Reyaz Ahamd Bhat <reyazbhat1881@gmail.com>
2025-10-16 11:08:41 +0530

    learned about the for loop and the break, continue statement

---
+++ itration/one.js
@@ -0,0 +1,51 @@
+// for (let i = 0; i <= 10; i++) {
+//     const element = i;
+//     if(i == 5)
+//     {
+//         console.log("5 is best number");
+        
+//     }
+//         console.log(element);
+    
+// }
+
+// for (let i = 0; i < 10; i++) {
+// if(i == 3){
+//             console.log("bst");
+            
+//         }
+// console.log(`outer loop value ${i}`);
+
+//     for (let j = 0; j < 10; j++) {
+
+        
+//         console.log(`inner loop value ${j} and outer lopp ${j}`);
+        
+        
+//     }
+    
+// }
+
+// let myArray = ["reyaz", "tufail", "buhan"]
+
+// for (let index = 0; index < myArray.length; index++) {
+//   let element = myArray[index];
+//     console.log(element);
+    
+    
+// }
+
+//**************** Break and Continue***************
+
+for (let index = 1; index <= 20; index++) {
+
+    if (index == 5) {
+
+        console.log(`detecteed 5`);
+        continue
+        
+        
+    }
+    console.log(`value of i is ${index}`);
+    
+}
