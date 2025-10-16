a4c90aa940dbe174b53300b60757c683addf3cf5
Reyaz Ahamd Bhat <reyazbhat1881@gmail.com>
2025-10-16 11:38:04 +0530

    learned about the do-while loop

---
+++ itration/do.js
@@ -0,0 +1,22 @@
+// let i = 0;
+// while (i <= 100) {
+//     console.log(`the value of the loop is ${i}`);
+// i =  i +2
+    
+// }
+
+// let myArray = ["reyaz", "tufail", "burhan"]
+
+// let arr = 0
+
+// while (arr < myArray.length) {
+//     console.log(`vaule is ${myArray[arr]}`);
+//     arr++
+// }
+
+let score = 11
+do{
+console.log(`score is ${score}`);
+score++
+} while (score <= 10);
+
