3a7b4d0aa7caa8cd7a738f085dc699664b449780
Reyaz Ahamd Bhat <reyazbhat1881@gmail.com>
2025-10-17 21:58:23 +0530

    learned about the for off and in loops

---
+++ itration/3.js
@@ -0,0 +1,17 @@
+//********** For OFF Loop *********
+
+const arr = [1,3,5,2,]
+
+for (const num of arr) {
+    console.log(num);
+    
+}
+
+
+// Maps
+
+const Map = new Map()
+
+Map.set('in', 'india')
+
+console.log(Map);
---
+++ itration/4.js
@@ -0,0 +1,22 @@
+// *********** Object ************8
+
+const myObject = {
+    js: 'javascript',
+    cpp: 'c++',
+    rs: 'ruby'
+
+}
+
+for (const key  in myObject) {
+    // console.log(key);
+
+}
+
+
+const prof = ['reyaz', 'tufial', "burhan"]
+
+for (const key in prof) {
+    
+    console.log(prof[key]);
+    
+}
