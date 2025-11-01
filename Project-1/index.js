4c36f0ff808ab64c52617feef28ae9adb2b8580a
Reyaz Ahamd Bhat <reyazbhat1881@gmail.com>
2025-11-01 21:28:24 +0530

    created a basic project

---
+++ Project-1/index.js
@@ -0,0 +1,17 @@
+const buttions = document.querySelectorAll('.button');
+const body = document.querySelectorAll('body')
+
+buttions.forEach(function(button){
+    console.log(button);
+    button.addEventListener('click', function(e){
+
+        console.log(e);
+        console.log(e.target);
+        if (e.target.id === 'grey'){
+            body.style.backgroundColor = e.target.id;
+        }
+        
+    
+    });
+});
+
