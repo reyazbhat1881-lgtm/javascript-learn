6f3cfa44ae5532210031deb0db3a1593468a8f2f
Reyaz Ahamd Bhat <reyazbhat1881@gmail.com>
2025-11-22 21:19:14 +0530

    promises and rejected, resolved

--- advance/promises.js
+++ advance/promises.js
@@ -23,4 +23,43 @@ new Promise(function(resolve, reject){
     
 })
 
+const promiseThree = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        resolve({username: "Reyaz", email: "reyazbhat1881@gmail.com"})
+
+    }, 1000)
+})
+
+promiseThree.then(function(user){
+console.log(user);
+
+})
+
+ const promiseFour = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = false
+        if (!error) {
+    resolve({error: "h"})
+
+        } else {
+            reject('error: wrong')
+        }
+    }, 1000)
+
+ })
+
+ promiseFour
+ .then((user) => {
+     console.log(user);
+     return user.username
+     
+ } )
+ .then((username) => {
+    console.log(username);
+    
+ })
+ .catch(function(error){
+    console.log(error);
+    
+ }).finally(() => console.log("the promise is resolved or rejected"))
  
