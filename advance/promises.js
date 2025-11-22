3a7f281cc970623ae52414fe7a40ba68161ac0f3
Reyaz Ahamd Bhat <reyazbhat1881@gmail.com>
2025-11-22 17:39:48 +0530

    new 2 promises

--- advance/promises.js
+++ advance/promises.js
@@ -2,6 +2,25 @@ const promiseNo1 = new Promise (function(resolve, reject){
 
     setTimeout(function(){
         console.log("Asyln");
+        resolve()
         
     }, 1000)
 })
+
+promiseNo1.then(function(){
+    console.log("Promise consumed");
+    
+})
+
+new Promise(function(resolve, reject){
+    setTimeout(function(){
+        console.log("async2");
+        resolve()
+        
+    }, 1000)
+}).then(function(){
+    console.log("async2 resolved");
+    
+})
+
+ 
